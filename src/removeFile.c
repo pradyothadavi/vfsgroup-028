0f1bba647ddcbd080657c1eafe13779d6be7450a
ritz.sharma6@gmail.com <ritz.sharma6@gmail.com@f73d97cc-2f44-211f-1de3-b3d45ccd6169>
2012-11-17 19:54:52 +0000

    removefile...

--- src/removeFile.c
+++ src/removeFile.c
@@ -1,5 +1,12 @@
 #include"mainHeader.h"
-
+#include"nAryTree.h"
+#include"fileDescriptor.h"
+#include"block.h"
+#include<global.h>
+#include<string.h>
+#include "limits.h"
+#include "fileSystemOps.h"
+#include"vfserrorcodes.h"
 
 /*
 Function Name: removefile
@@ -10,7 +17,7 @@ Return Type: It returns void
 
 void removefile ( char *P1 ){
 	/*
-		1. Search the BST with the entire path to get the file descriptor
+		1. Search the nary with the entire path to get the file descriptor
 		2. Delete that node from BST
 		3. Search the nary with the given iNode number from file descriptor and remove that node from nAry tree
 		4. Delete the given iNode number and delete that node from hashTable
@@ -18,11 +25,40 @@ void removefile ( char *P1 ){
 		6. Add the data block number to the dataBlock number free list
 
 	*/
-	struct fileDescriptor *fd=/* traverse BST with path  */;
-	/*call the delete function in BST with the path;*/
-	struct nAryTreeNode *deleteNode_nary=s_searchInode_NAryTreeNode(sPtr_rootNaryTree,fd->ui_inodeNo);
-	delete_nAry(deleteNode_nary);
+	struct nAryTreeNode *nary= NULL;
+	char *vfsLabel;
+	char *token;
+	struct fileDescriptor *fd;
+	char *ndir= NULL;
+	ndir=P1;
+	vfsLabel="demo";
+	nary=sPtr_rootNAryTree;
+	token=strtok(ndir,"/");
+	
+	while(token!=NULL)
+             {  
+                 
+             i_noOfCharacters=strlen(token)+1;
+             nary=s_searchNAryTreeNode(nary,token);
+
+              if(NULL==nary)
+                 {   
+                     printf(ERR_VFS_REMOVEFILE_01);
+                                     
+                 }
+                 
+               token=strtok(NULL,"/");
+            }
+
+	fd=nary->s_inode;
+	v_deleteNAryTreeNode(nary);
 	v_delete(fd->ui_inodeNo,fd->cptr_fileName[]); /*deleting from hash table */
 	s_inodeBlockFreeList=s_addFreeDataBlock(s_inodeBlockFreeList,fd->ui_inodeNo); /*add the inode number*/
 	s_dataBlockFreeList=s_addFreeDataBlock(s_dataBlockFreeList,fd->ui_locationDataBlockN[0]);/*add the data number*/
 }
+
+
+
+
+
+
